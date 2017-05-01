b4b6498cd57c7552bf937a25c4eb248ceabb8672
DESKTOP-DDHD624\Trolloks <rikuswlouw@gmail.com>
2017-05-01 14:06:48 +0200

    implemented services

---
+++ app/src/main/java/za/co/westcoastexplorers/exploreapp/Map.java
@@ -0,0 +1,68 @@
+package za.co.westcoastexplorers.exploreapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+
+import za.co.westcoastexplorers.R;
+
+/**
+ * Created by Trolloks on 5/1/2017.
+ */
+
+public class Map extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        // init database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("attractions");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                Object value = dataSnapshot.getValue(Object.class);
+                Log.d("DB", "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("DB", "Failed to read value.", error.toException());
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Yzerfontein, and move the camera.
+        LatLng sydney = new LatLng(-33.342506, 18.173862);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Welcome to Yzerfontein"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 16.0f));
+    }
+}
--- app/src/main/java/za/co/westcoastexplorers/exploreapp/Splash.java
+++ app/src/main/java/za/co/westcoastexplorers/exploreapp/Splash.java
@@ -1,7 +1,12 @@
 package za.co.westcoastexplorers.exploreapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+
+import za.co.westcoastexplorers.R;
 
 public class Splash extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        // set listeners
+        findViewById(R.id.loginbutton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Splash.this, Map.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
