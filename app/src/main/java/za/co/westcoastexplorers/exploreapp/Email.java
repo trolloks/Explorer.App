7f0dd5680d52ff17fb1be17541f0c4a88479ec58
Rikus Louw <rikus@blazingchilli.com>
2017-05-19 10:42:19 +0200

    Added some persistence and validation

--- app/src/main/java/za/co/westcoastexplorers/exploreapp/Email.java
+++ app/src/main/java/za/co/westcoastexplorers/exploreapp/Email.java
@@ -1,8 +1,11 @@
 package za.co.westcoastexplorers.exploreapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatCheckBox;
+import android.support.v7.widget.AppCompatEditText;
 import android.view.View;
 
 import za.co.westcoastexplorers.R;
@@ -12,15 +15,41 @@ import za.co.westcoastexplorers.R;
  */
 
 public class Email extends AppCompatActivity {
+    public static final String PREFS_NAME = "EmailSettings";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_email);
 
+        // Restore preferences
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        boolean skipEmail = settings.getBoolean("skipEmail", false);
+
+        if (skipEmail){
+            Intent intent = new Intent(Email.this, Map.class);
+            startActivity(intent);
+            finish();
+        }
+
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                AppCompatEditText email = (AppCompatEditText)findViewById(R.id.email);
+                if (email.getText().length() == 0){
+                    email.setError("You need to specify an email");
+                    return;
+                }
+
+                AppCompatCheckBox check = (AppCompatCheckBox)findViewById(R.id.check);
+
+                // set to cache
+                SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putBoolean("skipEmail", true);
+                editor.apply();
+
                 Intent intent = new Intent(Email.this, Map.class);
                 startActivity(intent);
                 finish();
--- app/src/main/java/za/co/westcoastexplorers/exploreapp/Tutorial.java
+++ app/src/main/java/za/co/westcoastexplorers/exploreapp/Tutorial.java
@@ -2,6 +2,7 @@ package za.co.westcoastexplorers.exploreapp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -17,6 +18,7 @@ import za.co.westcoastexplorers.exploreapp.utils.ViewGroupPagerAdapter;
  */
 
 public class Tutorial extends AppCompatActivity {
+    public static final String PREFS_NAME = "TutorialSettings";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +29,19 @@ public class Tutorial extends AppCompatActivity {
         ViewPager pager = (ViewPager)findViewById(R.id.pages);
         pager.setAdapter(adapter);
 
+        // Restore preferences
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        boolean skipTut = settings.getBoolean("skipTut", false);
+
+        if (skipTut){
+            goNext();
+        }
+
         ViewGroup tut1 = (ViewGroup) ((LayoutInflater) Tutorial.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.tutorial_1, null, false);
         tut1.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Tutorial.this, Email.class);
-                startActivity(intent);
-                finish();
+                goNext();
             }
         });
         adapter.addViewGroup(tut1);
@@ -42,9 +50,7 @@ public class Tutorial extends AppCompatActivity {
         tut2.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Tutorial.this, Email.class);
-                startActivity(intent);
-                finish();
+                goNext();
             }
         });
         adapter.addViewGroup(tut2);
@@ -53,14 +59,21 @@ public class Tutorial extends AppCompatActivity {
         tut3.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Tutorial.this, Email.class);
-                startActivity(intent);
-                finish();
+                goNext();
             }
         });
         adapter.addViewGroup(tut3);
+    }
 
+    private void goNext(){
+        // set to cache
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putBoolean("skipTut", true);
+        editor.apply();
 
-
+        Intent intent = new Intent(Tutorial.this, Email.class);
+        startActivity(intent);
+        finish();
     }
 }
